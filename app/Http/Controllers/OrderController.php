0a9d90da8eb64b748e13b9408d196ae09c4ac05b
Vincent <128141904+vinnindubi@users.noreply.github.com>
2025-08-20 11:35:33 +0300

    updated dynamic pricing

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Models\OrderItem;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -17,64 +18,78 @@ class OrderController extends Controller
         $data=Order::find($id);
         return response()->json([
             "message"=>"order returned ",
-            "data"=>$data->orderItems
+            "data"=>$data->products
         ]);
     }
     public function store(Request $request){
+        $overAllAmount=0;
+        $overAllquantity=0;
         $validated=$request->validate([
-            "quantity"=>"required|integer",
-            "amount"=>"required|integer",
             "items"=>"required|array",
             "items.*.product_id"=>"required|exists:products,id",
             "items.*.no_goods"=>"required|min:1",
-            "items.*.total_amount"=>"required|min:1"
         ]);
         $data=Order::create([
-            "quantity"=>$validated['quantity'],
-            'amount'=>$validated['amount'],
+            "quantity"=>0,
+            'amount'=>0,
             
         ]);
         /*her we are creating multiple record in the orderItem table .
         so we are collecting an array of products then we loop as we create them*/
         foreach( $validated['items'] as $item )
         {
-            $value=OrderItem::create([
+            $value=Product::findOrFail($item['product_id']);
+            $totalAmount= $value->price * $item['no_goods'];
+            OrderItem::create([
                 "order_id"=>$data->id,
                 "product_id"=>$item['product_id'],
                 "no_goods"=>$item['no_goods'],
-                "total_amount"=>$item['total_amount']
+                "total_amount"=>$totalAmount
             ]);
+
+            $overAllquantity+=$item['no_goods'];
+            $overAllAmount+=$totalAmount;
         }
+            $result=$data->update([
+                "quantity"=>$overAllquantity,
+            'amount'=>$overAllAmount,
+            ]);
+
         return response()->json([
             "message"=>"order created successfully",
-            "data"=>$value
+            "data"=>$result
         ]);
     }
     public function update(Request $request, $id){
+        $overAllQuantity=0;
+        $overAllAmount=0;
         $validated=$request->validate([
-            "quantity"=>"required|integer",
-            "amount"=>"required|integer",
             "items"=>"required|array",
             "items.*.product_id"=>"required|exists:products,id",
-            "items.*.no_goods"=>"required|min:1",
-            "items.*.total_amount"=>"required|min:1"
+            "items.*.no_goods"=>"required|integer|min:1",
         ]);
         $data=Order::find($id);
-        $data->update([
-            "quantity"=>$validated['quantity'],
-            'amount'=>$validated['amount'],
-        ]);
+        
         $value=OrderItem::where('order_id',$id);
         foreach($validated['items'] as $item){
+            $product=Product::findOrFail($item['product_id']);
+            $totalAmount=$product->price * $item['no_goods'];
             $value->update([
                 "order_id"=>$data->id,
                 "product_id"=>$item['product_id'],
                 "no_goods"=>$item['no_goods'],
-                "total_amount"=>$item['total_amount']
+                "total_amount"=>$totalAmount
             ]);
+            $overAllQuantity+= $item['no_goods'];
+            $overAllAmount+=$totalAmount;
         }
+        $result=$data->update([
+            "quantity"=>$overAllQuantity,
+            'amount'=>$overAllAmount,
+        ]);
         return response()->json([
-            "message"=>"order Updated Successfully"
+            "message"=>"order Updated Successfully",
+            "data"=>$result
         ]);
 
 
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,7 +8,7 @@ class Order extends Model
 {
     protected $fillable=['quantity','amount','customer_id','payment_id','payment_status'];
     public function products(){
-        return $this->belongsToMany(Product::class);
+        return $this->belongsToMany(Product::class,'order_items');
     }
     public function orderItems(){
         return $this->hasMany(OrderItem::class);
